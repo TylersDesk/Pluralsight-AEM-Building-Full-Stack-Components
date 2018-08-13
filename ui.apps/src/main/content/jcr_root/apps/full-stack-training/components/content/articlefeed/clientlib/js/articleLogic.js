ea78914dfb88e81b91aedd3cdd2a27a414abd4df
Tyler Maynard <tyler@hoodoo.digital>
2018-08-12 21:32:47 -0600

    Updated js to work with making an ajax call

--- ui.apps/src/main/content/jcr_root/apps/full-stack-training/components/content/articlefeed/clientlib/js/articleLogic.js
+++ ui.apps/src/main/content/jcr_root/apps/full-stack-training/components/content/articlefeed/clientlib/js/articleLogic.js
@@ -8,12 +8,17 @@
     console.log(fullStack.fakeJson);
 
     fullStack.init = function(){
-        $('.js-insert-articles').append(fullStack.template({articles:fullStack.fakeJson}));
+        // Changes this to use the endpoint data..
+        var articleFeeds = $(".article-feed").data('article-feed-json')
+
+        $.ajax({
+          url: articleFeeds
+        }).done(function(jsonResp) {
+            $('.js-insert-articles').append(fullStack.template({articles:jsonResp}));
+        });
     };
 
     //Fire it all
     fullStack.init();
 
 })(fullStack);
-
-console.log(jQuery);
